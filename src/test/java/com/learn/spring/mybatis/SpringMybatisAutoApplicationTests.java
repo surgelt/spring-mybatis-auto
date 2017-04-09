a880afb67aba053559578a566a8a1f93a0386e4d
surgelt <surge_lt@163.com>
2017-04-09 22:38:07 +0800

    home edit

--- src/test/java/com/learn/spring/mybatis/SpringMybatisAutoApplicationTests.java
+++ src/test/java/com/learn/spring/mybatis/SpringMybatisAutoApplicationTests.java
@@ -9,12 +9,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.learn.spring.mybatis.dao.LectureMapper;
 import com.learn.spring.mybatis.dao.RoleMapper;
+import com.learn.spring.mybatis.dao.StudentHealthMaleMapper;
 import com.learn.spring.mybatis.dao.StudentLectureMapper;
 import com.learn.spring.mybatis.dao.StudentMapper;
 import com.learn.spring.mybatis.dao.StudentSelfcardMapper;
 import com.learn.spring.mybatis.model.Lecture;
 import com.learn.spring.mybatis.model.Role;
 import com.learn.spring.mybatis.model.Student;
+import com.learn.spring.mybatis.model.StudentHealthMale;
 import com.learn.spring.mybatis.model.StudentLecture;
 import com.learn.spring.mybatis.model.StudentSelfcard;
 
@@ -32,6 +34,8 @@ public class SpringMybatisAutoApplicationTests {
 	private LectureMapper lectureMapper;
 	@Autowired
 	private StudentLectureMapper studentLectureMapper;
+	@Autowired
+	private StudentHealthMaleMapper studentHealthMaleMapper;
 
 	//@Test
 	public void contextLoads() {
@@ -43,29 +47,34 @@ public class SpringMybatisAutoApplicationTests {
 		System.out.println("==="+r.getRoleName());
 		
 	}
-	//@Test
+	@Test
 	public void insertStCard(){
 		StudentSelfcard sc = new StudentSelfcard();
 		sc.setStudentId(1);
 		sc.setNative("xinzhou");
 		studentSelfcardMapper.insert(sc);
 	}
-	
+	@Test
 	public void insertSt(){
 		Student s = new Student();
 		s.setCnname("cname");
 //		s.setId(1);
 		s.setNote("note");
-		s.setSex("nan");
+		s.setSex("1");
 		studentMapper.insert(s);
 	}
 	
-	//@Test
+	@Test
 	public void insertLecture(){
 		Lecture l = new Lecture();
 		l.setLectureName("数学");
 		l.setNote("shuxue");
 		lectureMapper.insert(l);
+		
+		Lecture l2 = new Lecture();
+		l2.setLectureName("英语");
+		l2.setNote("英语");
+		lectureMapper.insert(l2);
 	}
 	
 	@Test
@@ -76,6 +85,27 @@ public class SpringMybatisAutoApplicationTests {
 		sl.setStudentId(1);
 		studentLectureMapper.insert(sl);
 		
+		StudentLecture sl2 = new StudentLecture();
+		sl2.setGrade("89");
+		sl2.setLectureId(1);
+		sl2.setStudentId(1);
+		studentLectureMapper.insert(sl2);
+		
+	}
+	
+	
+	@Test
+	public void insertHealth(){
+		StudentHealthMale studentHealthMale = new StudentHealthMale();
+		studentHealthMale.setHeart("henhao");
+		studentHealthMale.setStudentId(1);
+		
+		StudentHealthMale studentHealthMale2 = new StudentHealthMale();
+		studentHealthMale2.setHeart("henhao2");
+		studentHealthMale2.setStudentId(1);
+		
+		studentHealthMaleMapper.insert(studentHealthMale);
+		studentHealthMaleMapper.insert(studentHealthMale2);
 	}
 	
 	@Test
@@ -86,5 +116,6 @@ public class SpringMybatisAutoApplicationTests {
 		System.out.println(ss);
 //		com.learn.spring.mybatis.dao.StudentSelfcardMapper.selectByStudentId();
 	}
+
 
 }
